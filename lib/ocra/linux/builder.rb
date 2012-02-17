587ba6c6018ea1447c979687839cd91ff6d49cfa
Duncan Mac-Vicar P <dmacvicar@suse.de>
2012-02-17 16:50:05 +0100

    log calls to the builder

--- lib/ocra/linux/builder.rb
+++ lib/ocra/linux/builder.rb
@@ -2,7 +2,16 @@ module Ocra
   class Builder
 
     def initialize(path, windowed)
+      File.open(path, "w") do |f|
+      end
+
+      yield(self)
+
     end
-    
+
+    def method_missing(name, *args)
+      puts "#{name.upcase} #{args.join(', ')}"
+    end
+
   end
 end
